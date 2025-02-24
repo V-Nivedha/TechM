3101246faf7ae045bd914ba9d8e9e5f9a9206c33
Nivedha V <115640211+V-Nivedha@users.noreply.github.com>
2025-02-24 21:54:28 +0530

    Delete 22.02.2025-24.02.2025 directory

--- 22.02.2025-24.02.2025/Task 1/Task 4/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-public class Main{
-    public static void main(String args[]){
-        int arr[]={23,56,89,90,34,35,5,9,12,45};
-        int copiedarry[]=Arrays.copyOfRange(arr,3,6);
-        System.out.println("orginalarray "+Arrays.toString(arr));
-        System.out.println("copiedarray "+Arrays.toString(copiedarry));
-    }
-}
