3ea33637c9f94275892dc1291026287faf9b04dd
Nivedha V <115640211+V-Nivedha@users.noreply.github.com>
2025-02-25 20:42:10 +0530

    Delete 25.02.2025 directory

--- 25.02.2025/Practice/index.js
+++
@@ -1,8 +0,0 @@
-function Football() {
-  const shoot = () => {
-    alert("good shot");
-  };
-  return <button onClick={shoot}>tap for shoot</button>;
-}
-const root = ReactDOM.createRoot(document.getElementById("root1"));
-root.render(<Football />);
