37813a8181988cbbe04e95fae80b8c2c7c6af5b5
Nivedha V <115640211+V-Nivedha@users.noreply.github.com>
2025-02-25 08:08:03 +0530

    Update InsertRow.java

--- 22.02.2025-24.02.2025/Task 8 - JDBC/Exp1/InsertRow.java
+++ 22.02.2025-24.02.2025/Task 8 - JDBC/Exp1/InsertRow.java
@@ -5,8 +5,8 @@ import java.sql.SQLException;
 
 public class InsertRow {
     private static final String URL = "jdbc:mysql://localhost:3306/Friends";
-    private static final String USER = "your_username";
-    private static final String PASSWORD = "your_password";
+    private static final String USER = "root";
+    private static final String PASSWORD = "090204";
 
     public static void main(String[] args) {
         String[][] data = {
